3742629c36c1da4efdb835c906db414dc88afe5b
Angelo Esguerra <aesgu001@ucr.edu>
2016-06-02 17:30:54 -0700

    problem 2 solved

--- test_sema_2a.c
+++ test_sema_2a.c
@@ -1,21 +1,24 @@
 #include "sema.h"
 #include "user.h"
 
-struct Semaphore tree;
-int i;
+struct Semaphore tree, mutex;
+int count, i;
 
 void monkey(void *arg_ptr);
 
 int main() {
+	count = i = 0;
+
 	sem_init(&tree);
 	sem_signal(&tree);
 	sem_signal(&tree);
 	sem_signal(&tree);
-	i = 0;
-	void *tid;
-	while (i++ < 3) {
-		tid = thread_create(monkey,(void *)0);
-		if (tid == 0) {
+
+	sem_init(&mutex);
+	sem_signal(&mutex);
+
+	while (i++ < 10) {
+		if (thread_create(monkey,(void *)0) == 0) {
 			printf(1,"thread_create() failed!\n");
 			exit();
 		}
@@ -27,7 +30,18 @@ int main() {
 
 void monkey(void *arg_ptr) {
 	sem_acquire(&tree);
+
+	sem_acquire(&mutex);
+	count++;
+	printf(1,"Number of monkeys: %d\n",count);
+	sem_signal(&mutex);
+
 	printf(1,"Grabbing coconut...\n");
+
+	sem_acquire(&mutex);
+	count--;
+	sem_signal(&mutex);
+
 	sem_signal(&tree);
 	texit();
 }
--- test_sema_2c.c
+++ test_sema_2c.c
@@ -1,23 +1,35 @@
 #include "sema.h"
 #include "user.h"
 
-struct Semaphore mutex1, mutex2, tree, climb;
-int moving_up = 0, moving_down = 0;
-int dominant_ready, i;
-struct queue dominant_q;
+struct Semaphore tree, climb, dominant_active;
+struct Semaphore mutex1, mutex2, mutex3, mutex4;
+int moving_up, moving_down, count;
+int dominant_count, dominant_ready;
 
 void monkey_help(void);
 void monkey(void *arg_ptr);
 void dominant_monkey(void *arg_ptr);
 
-int main() {
-	init_q(&dominant_q);
+int main(void) {
+	moving_up = moving_down = 0;
+	count = dominant_count = 0;
 	dominant_ready = 0;
+	
+	// moving up
 	sem_init(&mutex1);
 	sem_signal(&mutex1);
-
+	
+	// moving down
 	sem_init(&mutex2);
 	sem_signal(&mutex2);
+
+	// count
+	sem_init(&mutex3);
+	sem_signal(&mutex3);
+
+	// dominant_count & dominant_ready
+	sem_init(&mutex4);
+	sem_signal(&mutex4);
 
 	sem_init(&tree);
 	sem_signal(&tree);
@@ -27,17 +39,25 @@ int main() {
 	sem_init(&climb);
 	sem_signal(&climb);
 
-	i = 0;
-	void *tid;
-	while (i++ < 6) {
-		tid = thread_create(monkey,(void *)0);
-		if (tid == 0) {
-			printf(1,"thread_create() failed!\n");
-			exit();
-		}
+	sem_init(&dominant_active);
+
+	if (thread_create(monkey,(void *)0) == 0) {
+		printf(1,"thread_create() failed!\n");
+		exit();
 	}
-	tid = thread_create(dominant_monkey,(void *)0);
-	if (tid == 0) {
+	if (thread_create(monkey,(void *)0) == 0) {
+		printf(1,"thread_create() failed!\n");
+		exit();
+	}
+	if (thread_create(monkey,(void *)0) == 0) {
+		printf(1,"thread_create() failed!\n");
+		exit();
+	}
+	if (thread_create(dominant_monkey,(void *)0) == 0) {
+		printf(1,"thread_create() failed!\n");
+		exit();
+	}
+	if (thread_create(monkey,(void *)0) == 0) {
 		printf(1,"thread_create() failed!\n");
 		exit();
 	}
@@ -61,6 +81,11 @@ void monkey_help(void) {
 	if (moving_up == 0)
 		sem_signal(&climb);
 	sem_signal(&mutex1);
+
+	sem_acquire(&mutex3);
+	count++;
+	printf(1,"Number of monkeys: %d\n",count);
+	sem_signal(&mutex3);
 	printf(1,"Grabbing coconut...\n");
 
 	sem_acquire(&mutex2);
@@ -75,27 +100,46 @@ void monkey_help(void) {
 	if (moving_down == 0)
 		sem_signal(&climb);
 	sem_signal(&mutex2);
+
+	sem_acquire(&mutex3);
+	count--;
+	sem_signal(&mutex3);
 	printf(1,"Done!\n");
 
 	sem_signal(&tree);
 }
 
 void monkey(void *arg_ptr) {
+	sem_acquire(&mutex4);
 	if (dominant_ready) {
-		add_q(&dominant_q,getpid());
-		tsleep();
+		dominant_count++;
+		sem_signal(&mutex4);
+		printf(1,"Waiting...\n");
+		sem_acquire(&dominant_active);
+		printf(1,"Done waiting!\n");
 	}
+	else
+		sem_signal(&mutex4);
 	monkey_help();
 	texit();
 }
 
 void dominant_monkey(void *arg_ptr) {
-	printf(1,"\nDominant Monkey appears!\n");
+	sem_acquire(&mutex4);
 	dominant_ready = 1;
+	sem_signal(&mutex4);
+	printf(1,"Dominant Monkey ready!\n");
+	
 	monkey_help();
-	printf(1,"Dominant Monkey done!\n\n");
-	while (!empty_q(&dominant_q))
-		twakeup(pop_q(&dominant_q));
+	printf(1,"Dominant Monkey done!\n");
+
+	sem_acquire(&mutex4);
 	dominant_ready = 0;
+	while (dominant_count > 0) {
+		sem_signal(&dominant_active);
+		dominant_count = (dominant_count==0)? 0 : dominant_count - 1;
+	}
+	sem_signal(&mutex4);
+
 	texit();
 }
